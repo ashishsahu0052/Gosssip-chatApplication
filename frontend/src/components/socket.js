2a1e04904b9cbbe348658c421b857d3f66d0dd70
Ashish Sahu <ashishsahu0052@gmail.com>
2026-05-26 03:15:05 +0530

    backend ready

--- frontend/src/components/socket.js
+++ frontend/src/components/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000", {
+export const socket = io("https://gosssip-chatapplication.onrender.com", {
   autoConnect: false,
 });
